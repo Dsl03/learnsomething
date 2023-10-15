cd6d7d2bf4601a32d5bfde94091069ba9239e454
Daniel Lee <lee.seung.dan@gmail.com>
2023-10-15 12:43:38 -0500

    Created PricingUtil C++ module with calc and get functions + added corresponding unit tests

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,13 @@
 #include "pricingutil.h"
+
+PricingUtil::PricingUtil() {};
+
+float PricingUtil::calcVal(float previousPrice, float interest, float oleoConstant) {
+    float output = (previousPrice * (0.9 + interest)) * oleoConstant;
+    this->val = output;
+    return output;
+}
+
+float PricingUtil::getVal() {
+    return this->val;
+}
--- tst/test_pricingutil.cpp
+++ tst/test_pricingutil.cpp
@@ -1,5 +1,30 @@
 #include <gtest/gtest.h>
+#include "pricingutil.h"
 
 TEST(sampleTest, sample) {
     EXPECT_EQ(4, 4);
 }
+
+TEST(PricingUtilTest, TestFormula) {
+    PricingUtil pricingUtil = PricingUtil();
+
+    float previousPrice = 100;
+    float interest = 0.1;
+    float oleoConstant = 0.5;
+
+    float expected = (previousPrice * (0.9 + interest)) * oleoConstant;
+    float actual = pricingUtil.calcVal(previousPrice, interest, oleoConstant);
+
+    EXPECT_EQ(expected, actual);
+
+    // Test that value is stored in the class
+    EXPECT_NEAR(pricingUtil.getVal(), actual, 1e-4);
+    EXPECT_NEAR(pricingUtil.calcVal(40, 0.1, 3), 120, 1e-4);
+    EXPECT_NEAR(pricingUtil.getVal(), 120, 1e-4);
+    EXPECT_NEAR(pricingUtil.calcVal(60, 0.35, 2), 150, 1e-4);
+    EXPECT_NEAR(pricingUtil.getVal(), 150, 1e-4);
+
+}
+
+
+
